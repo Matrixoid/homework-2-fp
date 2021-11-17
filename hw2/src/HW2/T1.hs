6d2dad00f5105a55cf9403b7bb7dff3daa9c8738
Lord Matrixoid <mr.art.pro@mail.ru>
2021-11-17 03:02:14 +0300

    sa

--- hw2/src/HW2/T1.hs
+++ hw2/src/HW2/T1.hs
@@ -1,165 +1,78 @@
-<<<<<<< HEAD
 module HW2.T1
-  ( Annotated (..)
-  , Except (..)
-  , Fun (..)
-  , List (..)
-  , Option (..)
-  , Pair (..)
-  , Prioritised (..)
-  , Quad (..)
-  , Stream (..)
-  , Tree (..)
-  , mapAnnotated
-  , mapExcept
-  , mapFun
-  , mapList
+  ( Option(..)
+  , Pair(..)
+  , Quad(..)
+  , Annotated(..)
+  , Except(..)
+  , Prioritised(..)
+  , Stream(..)
+  , List(..)
+  , Fun(..)
+  , Tree(..)
   , mapOption
   , mapPair
-  , mapPrioritised
   , mapQuad
-  , mapStream
-  , mapTree
-  ) where
-
-data Option a = None | Some a
-
-data Pair a = P a a
-
-data Quad a = Q a a a a
-
-data Annotated e a = a :# e
-infix 0 :#
-
-data Except e a = Error e | Success a
-
-data Prioritised a = Low a | Medium a | High a
-
-data Stream a = a :> Stream a
-infixr 5 :>
-
-data List a = Nil | a :. List a
-infixr 5 :.
-
-newtype Fun i a = F (i -> a)
-
-data Tree a = Leaf | Branch (Tree a) a (Tree a)
-
-mapOption :: (a -> b) -> (Option a -> Option b)
-mapOption fun None     = None
-mapOption fun (Some o) = Some (fun o)
-
-mapPair :: (a -> b) -> (Pair a -> Pair b)
-mapPair fun (P a b) = P (fun a) (fun b)
-
-mapQuad :: (a -> b) -> (Quad a -> Quad b)
-mapQuad fun (Q a b c d) = Q (fun a) (fun b) (fun c) (fun d)
-
-mapAnnotated :: (a -> b) -> (Annotated e a -> Annotated e b)
-mapAnnotated fun (val :# def) = (fun val) :# def
-
-mapExcept :: (a -> b) -> (Except e a -> Except e b)
-mapExcept fun (Success suc) = Success (fun suc)
-mapExcept fun (Error err)   = Error err
-
-mapPrioritised :: (a -> b) -> (Prioritised a -> Prioritised b)
-mapPrioritised fun (Low p)    = Low (fun p)
-mapPrioritised fun (Medium p) = Medium (fun p)
-mapPrioritised fun (High p)   = High (fun p)
-
-mapStream :: (a -> b) -> (Stream a -> Stream b)
-mapStream fun (s :> sT) = fun s :> mapStream fun sT
-
-mapList :: (a -> b) -> (List a -> List b)
-mapList fun (h :. t) =  fun h :. mapList fun t
-mapList fun Nil      = Nil
-
-mapFun :: (a -> b) -> (Fun i a -> Fun i b)
-mapFun fun (F f) = F (\x -> fun (f x))
-
-mapTree :: (a -> b) -> (Tree a -> Tree b)
-mapTree fun Leaf           = Leaf
-mapTree fun (Branch l v r) = Branch (mapTree fun l) (fun v) (mapTree fun r)
-=======
-module HW2.T1
-  ( Annotated (..)
-  , Except (..)
-  , Fun (..)
-  , List (..)
-  , Option (..)
-  , Pair (..)
-  , Prioritised (..)
-  , Quad (..)
-  , Stream (..)
-  , Tree (..)
   , mapAnnotated
   , mapExcept
-  , mapFun
-  , mapList
-  , mapOption
-  , mapPair
   , mapPrioritised
-  , mapQuad
   , mapStream
+  , mapList
+  , mapFun
   , mapTree
-  ) where
+  )where
 
-data Option a = None | Some a
-
-data Pair a = P a a
-
-data Quad a = Q a a a a
-
-data Annotated e a = a :# e
+data Option a = None | Some a deriving Show
+data Pair a = P a a deriving Show
+data Quad a = Q a a a a deriving Show
+data Annotated e a = a :# e deriving Show
 infix 0 :#
-
-data Except e a = Error e | Success a
-
-data Prioritised a = Low a | Medium a | High a
-
-data Stream a = a :> Stream a
+data Except e a = Error e | Success a deriving Show
+data Prioritised a = Low a | Medium a | High a deriving Show
+data Stream a = a :> (Stream a) deriving Show
 infixr 5 :>
-
-data List a = Nil | a :. List a
+data List a = Nil | a :. List a deriving Show
 infixr 5 :.
-
-newtype Fun i a = F (i -> a)
-
-data Tree a = Leaf | Branch (Tree a) a (Tree a)
+data Fun i a = F (i -> a)
+data Tree a = Leaf | Branch (Tree a) a (Tree a) deriving Show
 
 mapOption :: (a -> b) -> (Option a -> Option b)
-mapOption fun None     = None
-mapOption fun (Some o) = Some (fun o)
+mapOption _ None = None
+mapOption function (Some opt) = Some (function opt)
 
 mapPair :: (a -> b) -> (Pair a -> Pair b)
-mapPair fun (P a b) = P (fun a) (fun b)
+mapPair function (P num1 num2) = P (function num1) (function num2)
 
 mapQuad :: (a -> b) -> (Quad a -> Quad b)
-mapQuad fun (Q a b c d) = Q (fun a) (fun b) (fun c) (fun d)
+mapQuad function (Q num1 num2 num3 num4) = Q (function num1)
+                                             (function num2)
+                                             (function num3)
+                                             (function num4)
 
 mapAnnotated :: (a -> b) -> (Annotated e a -> Annotated e b)
-mapAnnotated fun (val :# def) = (fun val) :# def
+mapAnnotated function (a :# e) = function a :# e
 
 mapExcept :: (a -> b) -> (Except e a -> Except e b)
-mapExcept fun (Success suc) = Success (fun suc)
-mapExcept fun (Error err)   = Error err
+mapExcept function (Error err) = Error err
+mapExcept function (Success res) = Success (function res)
 
 mapPrioritised :: (a -> b) -> (Prioritised a -> Prioritised b)
-mapPrioritised fun (Low p)    = Low (fun p)
-mapPrioritised fun (Medium p) = Medium (fun p)
-mapPrioritised fun (High p)   = High (fun p)
+mapPrioritised function (Low low) = Low (function low)
+mapPrioritised function (Medium med) = Medium (function med)
+mapPrioritised function (High high) = High (function high) 
 
 mapStream :: (a -> b) -> (Stream a -> Stream b)
-mapStream fun (s :> sT) = fun s :> mapStream fun sT
+mapStream function (streamBegin :> streamTail) = function streamBegin 
+                                               :> mapStream function streamTail
 
 mapList :: (a -> b) -> (List a -> List b)
-mapList fun (h :. t) =  fun h :. mapList fun t
-mapList fun Nil      = Nil
+mapList _ Nil = Nil
+mapList function (listHead :. listTail) = function listHead :. mapList function listTail
 
 mapFun :: (a -> b) -> (Fun i a -> Fun i b)
-mapFun fun (F f) = F (\x -> fun (f x))
+mapFun function1 (F function2) = F (function1 . function2)
 
 mapTree :: (a -> b) -> (Tree a -> Tree b)
-mapTree fun Leaf           = Leaf
-mapTree fun (Branch l v r) = Branch (mapTree fun l) (fun v) (mapTree fun r)
->>>>>>> a48a6408b5947c31b98bae09c179de7e82b0df44
+mapTree _ Leaf = Leaf
+mapTree function (Branch left element right) = Branch (mapTree function left) 
+                                                      (function element)
+                                                      (mapTree function right)
--- hw2/src/HW2/T2.hs
+++ hw2/src/HW2/T2.hs
@@ -1,76 +1,84 @@
-<<<<<<< HEAD
 module HW2.T2
-  ( distAnnotated
-  , distExcept
-  , distFun
-  , distList
-  , distOption
-  , distPair
-  , distPrioritised
-  , distQuad
-  , distStream
-  , wrapAnnotated
-  , wrapExcept
-  , wrapFun
-  , wrapList
+  ( distOption
   , wrapOption
+  , distPair
   , wrapPair
-  , wrapPrioritised
+  , distQuad
   , wrapQuad
+  , distAnnotated
+  , wrapAnnotated
+  , distExcept
+  , wrapExcept
+  , distPrioritised
+  , wrapPrioritised
+  , distStream
   , wrapStream
-  ) where
+  , distList
+  , wrapList
+  , distFun
+  , wrapFun
+  )where
 
-import HW2.T1 (Annotated ((:#)), Except (Error, Success), Fun (F), List (Nil, (:.)),
-               Option (None, Some), Pair (P), Prioritised (High, Low, Medium), Quad (Q),
-               Stream ((:>)))
+import HW2.T1 ( Option(..)
+              , Pair(..)
+              , Quad(..)
+              , Annotated(..)
+              , Except(..)
+              , Prioritised(..)
+              , Stream(..)
+              , List(..)
+              , Fun(..))
+import Data.Semigroup hiding (Option)
+import Data.Monoid hiding ((<>))
 
 distOption :: (Option a, Option b) -> Option (a, b)
+distOption (None, _) = None
+distOption (_, None) = None
 distOption (Some a, Some b) = Some (a, b)
-distOption _                = None
 
 distPair :: (Pair a, Pair b) -> Pair (a, b)
 distPair (P a b, P c d) = P (a, c) (b, d)
 
 distQuad :: (Quad a, Quad b) -> Quad (a, b)
-distQuad (Q a b c d, Q e f g h) = Q (a, e) (b, f) (c, g) (d, h)
+distQuad (Q a1 b1 c1 d1, Q a2 b2 c2 d2) = Q (a1, a2) (b1, b2) (c1, c2) (d1, d2)
 
 distAnnotated :: Semigroup e => (Annotated e a, Annotated e b) -> Annotated e (a, b)
-distAnnotated (aV :# aDef, bV :# bDef) = (aV, bV) :# (bDef <> aDef)
+distAnnotated (a1 :# e1, a2 :# e2) = (a1, a2) :# (e1 <> e2)
 
 distExcept :: (Except e a, Except e b) -> Except e (a, b)
-distExcept (Error aErr, b)        = Error aErr
-distExcept (a, Error bErr)        = Error bErr
+distExcept (Error e, _) = Error e
+distExcept (_, Error e) = Error e
 distExcept (Success a, Success b) = Success (a, b)
 
 distPrioritised :: (Prioritised a, Prioritised b) -> Prioritised (a, b)
-distPrioritised (Low a,    Low b)    = Low (a, b)
-distPrioritised (Low a,    Medium b) = Medium (a, b)
-distPrioritised (Medium a, Low b)    = Medium (a, b)
+distPrioritised (Low a, Low b) = Low (a, b)
+distPrioritised (Low a, Medium b) = Medium (a, b)
+distPrioritised (Low a, High b) = High (a, b)
+distPrioritised (Medium a, Low b) = Medium (a, b)
 distPrioritised (Medium a, Medium b) = Medium (a, b)
-distPrioritised (High a,   Low b)    = High (a, b)
-distPrioritised (Low a,    High b)   = High (a, b)
-distPrioritised (High a,   Medium b) = High (a, b)
-distPrioritised (Medium a, High b)   = High (a, b)
-distPrioritised (High a,   High b)   = High (a, b)
+distPrioritised (Medium a, High b) = High (a, b)
+distPrioritised (High a, Low b) = High (a, b)
+distPrioritised (High a, Medium b) = High (a, b)
+distPrioritised (High a, High b) = High (a, b)
 
 distStream :: (Stream a, Stream b) -> Stream (a, b)
-distStream (a :> aT, b :> bT) = (a, b) :> distStream (aT, bT)
-
-combineLists :: List a -> List a -> List a
-combineLists (a :. aT) b = a :. (combineLists aT b)
-combineLists Nil b       = b
-
-combineElement :: a -> List b -> List (a, b)
-combineElement a (b :. bT) = (a, b) :. combineElement a bT
-combineElement a Nil       = Nil
+distStream (a :> b, c :> d) = (a, c) :> distStream (b, d)
 
 distList :: (List a, List b) -> List (a, b)
-distList (Nil, b)       = Nil
-distList (a :. aT, Nil) = Nil
-distList (a :. aT, b)   = combineLists (combineElement a b) (distList (aT, b))
+distList (Nil, _) = Nil
+distList (_, Nil) = Nil
+distList (a :. b, c :. d) = (a, c) :. listConcat (helper (a :. b, d)) (distList (b, c :. d))
+  where
+    listConcat :: List a -> List a -> List a
+    listConcat Nil l2 = l2
+    listConcat (a1 :. l1t) l2 = a1 :. listConcat l1t l2
+    helper :: (List a, List b) -> List (a, b)
+    helper (Nil, _) = Nil
+    helper (_, Nil) = Nil
+    helper (a :. b, c :. d) = (a, c) :. helper (a :. b, d)
 
 distFun :: (Fun i a, Fun i b) -> Fun i (a, b)
-distFun (F aFun, F bFun) = F (\x -> (aFun x, bFun x))
+distFun (F f, F g) = F (\x -> (f x, g x))
 
 wrapOption :: a -> Option a
 wrapOption = Some
@@ -81,8 +89,8 @@ wrapPair a = P a a
 wrapQuad :: a -> Quad a
 wrapQuad a = Q a a a a
 
-wrapAnnotated   :: Monoid e => a -> Annotated e a
-wrapAnnotated val = val :# mempty
+wrapAnnotated :: Monoid e => a -> Annotated e a
+wrapAnnotated a = a :# mempty
 
 wrapExcept :: a -> Except e a
 wrapExcept = Success
@@ -91,111 +99,10 @@ wrapPrioritised :: a -> Prioritised a
 wrapPrioritised = Low
 
 wrapStream :: a -> Stream a
-wrapStream s = s :> wrapStream s
+wrapStream a = a :> wrapStream a
 
 wrapList :: a -> List a
-wrapList h = h :. Nil
+wrapList a = a :. Nil
 
 wrapFun :: a -> Fun i a
-wrapFun a = F (\x -> a)
-=======
-module HW2.T2
-  ( distAnnotated
-  , distExcept
-  , distFun
-  , distList
-  , distOption
-  , distPair
-  , distPrioritised
-  , distQuad
-  , distStream
-  , wrapAnnotated
-  , wrapExcept
-  , wrapFun
-  , wrapList
-  , wrapOption
-  , wrapPair
-  , wrapPrioritised
-  , wrapQuad
-  , wrapStream
-  ) where
-
-import HW2.T1 (Annotated ((:#)), Except (Error, Success), Fun (F), List (Nil, (:.)),
-               Option (None, Some), Pair (P), Prioritised (High, Low, Medium), Quad (Q),
-               Stream ((:>)))
-
-distOption :: (Option a, Option b) -> Option (a, b)
-distOption (Some a, Some b) = Some (a, b)
-distOption _                = None
-
-distPair :: (Pair a, Pair b) -> Pair (a, b)
-distPair (P a b, P c d) = P (a, c) (b, d)
-
-distQuad :: (Quad a, Quad b) -> Quad (a, b)
-distQuad (Q a b c d, Q e f g h) = Q (a, e) (b, f) (c, g) (d, h)
-
-distAnnotated :: Semigroup e => (Annotated e a, Annotated e b) -> Annotated e (a, b)
-distAnnotated (aV :# aDef, bV :# bDef) = (aV, bV) :# (bDef <> aDef)
-
-distExcept :: (Except e a, Except e b) -> Except e (a, b)
-distExcept (Error aErr, b)        = Error aErr
-distExcept (a, Error bErr)        = Error bErr
-distExcept (Success a, Success b) = Success (a, b)
-
-distPrioritised :: (Prioritised a, Prioritised b) -> Prioritised (a, b)
-distPrioritised (Low a,    Low b)    = Low (a, b)
-distPrioritised (Low a,    Medium b) = Medium (a, b)
-distPrioritised (Medium a, Low b)    = Medium (a, b)
-distPrioritised (Medium a, Medium b) = Medium (a, b)
-distPrioritised (High a,   Low b)    = High (a, b)
-distPrioritised (Low a,    High b)   = High (a, b)
-distPrioritised (High a,   Medium b) = High (a, b)
-distPrioritised (Medium a, High b)   = High (a, b)
-distPrioritised (High a,   High b)   = High (a, b)
-
-distStream :: (Stream a, Stream b) -> Stream (a, b)
-distStream (a :> aT, b :> bT) = (a, b) :> distStream (aT, bT)
-
-combineLists :: List a -> List a -> List a
-combineLists (a :. aT) b = a :. (combineLists aT b)
-combineLists Nil b       = b
-
-combineElement :: a -> List b -> List (a, b)
-combineElement a (b :. bT) = (a, b) :. combineElement a bT
-combineElement a Nil       = Nil
-
-distList :: (List a, List b) -> List (a, b)
-distList (Nil, b)       = Nil
-distList (a :. aT, Nil) = Nil
-distList (a :. aT, b)   = combineLists (combineElement a b) (distList (aT, b))
-
-distFun :: (Fun i a, Fun i b) -> Fun i (a, b)
-distFun (F aFun, F bFun) = F (\x -> (aFun x, bFun x))
-
-wrapOption :: a -> Option a
-wrapOption = Some
-
-wrapPair :: a -> Pair a
-wrapPair a = P a a
-
-wrapQuad :: a -> Quad a
-wrapQuad a = Q a a a a
-
-wrapAnnotated   :: Monoid e => a -> Annotated e a
-wrapAnnotated val = val :# mempty
-
-wrapExcept :: a -> Except e a
-wrapExcept = Success
-
-wrapPrioritised :: a -> Prioritised a
-wrapPrioritised = Low
-
-wrapStream :: a -> Stream a
-wrapStream s = s :> wrapStream s
-
-wrapList :: a -> List a
-wrapList h = h :. Nil
-
-wrapFun :: a -> Fun i a
-wrapFun a = F (\x -> a)
->>>>>>> a48a6408b5947c31b98bae09c179de7e82b0df44
+wrapFun a = F (const a)
